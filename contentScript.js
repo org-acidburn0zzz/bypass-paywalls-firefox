6c41cb7b85cc47ccf4e576f1d9ecee37fe03b56c
Davide Saurino <davide.saurino@immobiliare.it>
2020-04-01 19:37:27 +0200

    fix(rep.repubblica.it): fix infinite loop

--- contentScript.js
+++ contentScript.js
@@ -53,8 +53,10 @@ if (window.location.href.indexOf("bizjournals.com") !== -1) {
         }
     }
 } else if (location.hostname.endsWith('rep.repubblica.it')) {
-  if (location.href.includes('/pwa/')) {
-        location.href = location.href.replace('/pwa/', '/ws/detail/');
+    if (location.href.includes('/pwa/')) {
+        setTimeout(function () {
+            window.location.href = window.location.href.replace('/pwa/', '/ws/detail/');
+        }, 400);
     }
     if (location.href.includes('/ws/detail/')) {
         const paywall = document.querySelector('.paywall[subscriptions-section="content"]');
